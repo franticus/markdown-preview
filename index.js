b11bbb8d8d3ba37aad66be64794e0b1cefa70b6e
S.Zelensky <zelenstein@gmail.com>
2023-07-26 23:45:32 +0300

    devider

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ source.addEventListener('keyup', () => {
   const newText = marked.parse(source.value);
   preview.innerHTML = newText;
 });
+
+const onMouseMove = (event) => {
+  const { left, width } = container.getBoundingClientRect();
+  const { clientX } = event;
+  source.style.width = `${clientX - left}px`;
+  preview.style.width = `${left + width - clientX}px`;
+};
+
+const onMouseUp = () => {
+  document.removeEventListener('mousemove', onMouseMove);
+  document.removeEventListener('mouseup', onMouseUp);
+};
+
+const onMouseDown = () => {
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
+
+devider.addEventListener('mousedown', onMouseDown);
